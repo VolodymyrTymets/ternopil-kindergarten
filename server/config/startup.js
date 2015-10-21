b9e184d5895a3f99890355f6405bed4bbba6a58a
vova tymets <volodymyrtymets@gmail.com>
2015-10-21 13:04:19 +0300

    add vk authorizations

--- server/config/startup.js
+++ server/config/startup.js
@@ -1,4 +1,14 @@
 Meteor.startup(function () {
+    ServiceConfiguration.configurations.remove({
+        service: 'vk'
+    });
+
+    ServiceConfiguration.configurations.insert({
+        service: 'vk',
+        appId:   Meteor.settings.appIdVk,      // Your app id
+        secret:  Meteor.settings.secretVk  // Your app secret
+    });
+
     if(!Meteor.users.findOne()){
         Accounts.createUser({
             username:'admin',
